956fb5fbc5113476794190d37606b5b27ddd659c
pdmm2 <pdmm2@DESKTOP-J73R6IS>
2023-01-08 15:09:25 +0100

    COMMIT 3

--- src/robot/Robot.java
+++ src/robot/Robot.java
@@ -1,5 +1,7 @@
 package robot;
 
+import java.util.concurrent.TimeUnit;
+
 public class Robot {
 
 	//Creas todos los atributos
@@ -18,23 +20,71 @@ public class Robot {
 		
 		
 	}
-	/*
-	public void  Luchar() {
+	
+	public void  Luchar(Robot Terminator) throws InterruptedException {
 		
-		while (puntosVida >= 0) {
+		//Hacer un bucle para que mientras tengan vida se siga haciendo todo lo de dentro
+		while (puntosVida > 0 && Terminator.puntosVida > 0) {
 			
-			if (ataque > defensa)  {
+			//Sacas un numero aleatorio para que sea el ataque que va a hacer
+			int Ataque1 = (int) (Math.random()*101);
+			
+			//Si el ataque es mayor que la defensa y el ambos tienen puntos de vida empieza la lucha
+			if (Ataque1 > Terminator.defensa && this.puntosVida > 0 && Terminator.puntosVida > 0)  {
 				
-				puntosVida = puntosVida - ataque;
+				TimeUnit.SECONDS.sleep(3);
+				//System.out.println(Ataque1);
+						
+				if (Terminator.puntosVida < Ataque1)  {
+					
+					Ataque1 = Terminator.puntosVida;
+					
+				}
+				
+				Terminator.puntosVida -= Ataque1;
+				//System.out.println(puntosVida);
+			}
+			
+			else if (Ataque1 <= Terminator.defensa && this.puntosVida > 0 && Terminator.puntosVida > 0) {
+
+			}
+			
+			
+		
+			int Ataque2 = (int) (Math.random()*101);
+			
+			if (Ataque2 > this.defensa && this.puntosVida > 0 && Terminator.puntosVida > 0)  {
+				
+				TimeUnit.SECONDS.sleep(3);
+				//System.out.println(Ataque2);
+						
+				if (this.puntosVida < Ataque2)  {
+					
+					Ataque2 = this.puntosVida;
+				}
+				
+				this.puntosVida -= Ataque2;
+				//System.out.println(puntosVida);
 				
 			}
 			
-			else if (ataque < defensa ) {
-				
+			else if (Ataque2 <= this.defensa && this.puntosVida > 0 && Terminator.puntosVida > 0) {
+
 			}
 		}
+		
+		if (this.puntosVida <= 0) {
+			
+			System.out.println(name + " Perdió");
+		}
+		
+		if (Terminator.puntosVida <= 0) {
+			
+			System.out.println(Terminator + " Perdió");
+		}
+		
 	}	
-	 */
+	 
 	
 	//creas los getters y setters y el toString de todos los atributos
 	public String getName() {
@@ -61,11 +111,13 @@ public class Robot {
 	public void setDefensa(int defensa) {
 		this.defensa = defensa;
 	}
+
 	@Override
 	public String toString() {
 		return "Robot [name=" + name + ", puntosVida=" + puntosVida + ", ataque=" + ataque + ", defensa=" + defensa
-				+ "]";
+				+ "]" ;
 	}
+	
 	
 	
 	
--- src/robot/Start.java
+++ src/robot/Start.java
@@ -2,7 +2,7 @@ package robot;
 
 public class Start {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
 		Robot robot1 = new Robot("Terminator");
@@ -13,7 +13,7 @@ public class Start {
 		
 		System.out.println(robot2);
 		
-		
+		robot1.Luchar(robot2);
 		
 	}
 
